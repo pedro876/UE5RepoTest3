2bb9f4025ea0823fbd6d03386cbc7a362aa57918
Pedro Casas Martínez <pedrocasasdev@gmail.com>
2023-09-24 19:13:39 +0200

    Created VSCode Project

---
+++ Source/UERepoTest3/UERepoTest3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UERepoTest3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UERepoTest3, "UERepoTest3" );
